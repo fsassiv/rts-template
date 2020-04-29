a586b7d24aca6dc8db0770e14c087f6a0bc6d4b4
FLAVIO SANTOS DE ANDRADE <flavio.ssiv@gmail.com>
2020-04-29 08:30:09 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React, { useEffect } from 'react';
 import './App.scss';
 
 const App = () => {
-  useEffect(() => {
-    console.log('Hello');
-  }, []);
-
   return <div>Hello world</div>;
 };
 
