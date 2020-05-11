74d1c4062a50c39aeec0f1c6f904f63f9babe708
FLAVIO SANTOS DE ANDRADE <flavio.ssiv@gmail.com>
2020-05-10 22:28:49 -0300

    Debug config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
 const App = () => {
-  return <div>Hello world</div>;
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    console.log('Mounted again');
+    setValue('Sinner');
+  }, []);
+
+  return <div>Hello world {value}</div>;
 };
 
 export default App;
